ab3ab195839e581d48fa9040def81b84ea0222fd
rozer <shubhamsinha07084@gmail.com>
2024-11-27 17:10:00 +0530

    defined layout + setup clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +17,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <Header />
+
+          <div className="flex min-h-screen">
+            {/* Sidebar on left side */}
+            <Sidebar />
+
+            {/* page.tsx on right side */}
+            <div className="flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide">
+              {children}
+            </div>
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
